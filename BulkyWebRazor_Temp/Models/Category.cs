e13e5a539785b7d33553d9b623393342fd13b6bb
Phạm Xuân Phong <phamxuanphong09012003@gmail.com>
2024-07-13 15:59:28 +0700

    Update 56

---
+++ BulkyWebRazor_Temp/Models/Category.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+namespace BulkyWebRazor_Temp.Models
+{
+    public class Category
+    {
+
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(30, ErrorMessage = "Vui Lòng Điền tên")]
+        [DisplayName("Category Name")]
+        public string Name { get; set; }
+        [DisplayName("Display Order")]
+        [Range(1, 100, ErrorMessage = "Display Order từ 1-100")]
+        public int DisplayOrder { get; set; }
+
+    }
+}
